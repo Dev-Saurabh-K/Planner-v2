e74a86fd0717beac172d4faa69fce9b2b081f98c
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2025-10-01 09:42:18 +0530

    api fic 4

--- app/layout.js
+++ app/layout.js
@@ -19,9 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="bg-black">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
